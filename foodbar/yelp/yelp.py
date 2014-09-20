41b40f794c81be0ad0500bfb7c434e766044be7f
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2014-09-20 12:28:20 -0400

    more changes to yelp interface

--- foodbar/yelp/yelp.py
+++ foodbar/yelp/yelp.py
@@ -21,12 +21,17 @@ class Yelp:
 		self.token_secret='RdiSAYCG_m0VS1nIK_lMmpLymLE'
 		self.auth = OAuth1(self.consumer_key, self.consumer_secret, self.token, self.token_secret)
 
-	def search(self, params):
+
+	def search(self, params, count=False):
 		search_url = self.base_url
 		for key in params.keys():
 			search_url+=key+'='+params[key].replace(' ', '+')+'&'
 		response = requests.get(search_url, auth=self.auth)
 		if response.status_code != 200:
 			raise
+		if count:
+			return response.json()
 		return response.json()['businesses']
 
+	def count(self, params):
+		return self.search(params, True)['total']
